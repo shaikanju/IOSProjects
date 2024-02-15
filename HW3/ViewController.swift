a6f42b49b84f9551fb4759e4bd4e1dee3ed0be61
CDMStudent <cdmstudent@loop-depaulsecure-231-154.depaulsecure-student.depaul.edu>
2024-02-15 13:06:23 -0600

    Initial commit

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -8,12 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    var currentOperand: String = ""
+        
+    var initialsum: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        resultLabel.text="0"
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        guard let digit = sender.titleLabel?.text else { return }
+        appendDigitToOperand(digit)
+        
+        
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        clearResultLabel()
+        clearSum()
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        addInputToSum()
 
+    }
+    
+    @IBAction func equalsButtonTapped(_ sender: UIButton) {
+        computeSum()
+    }
+    
+    func appendDigitToOperand(_ digit: String) {
+                    currentOperand += digit
+        
+        updateResultLabel()
+    }
+    
+    func clearResultLabel() {
+        currentOperand = ""
+        resultLabel.text="0"    }
+    
+    func clearSum() {
+        
+        initialsum = 0
+    }
+    
+    func addInputToSum() {
+        guard let operand = Int(currentOperand) else { return }
+        initialsum += operand
+                currentOperand = "0" // Reset the current operand for the next input
+        
+        updateResultLabel()
+    }
 
+   
+    func computeSum() {
+        guard let operand = Int(currentOperand) else { return }
+        initialsum += operand
+        
+        resultLabel.text=String(initialsum)
+
+        
+    }
+    
+    
+    
+    func updateResultLabel() {
+        resultLabel.text = currentOperand
+    }
+    
 }
+
+
 
--- HW3UITests/HW3UITests.swift
+++ HW3UITests/HW3UITests.swift
@@ -9,33 +9,306 @@ import XCTest
 
 final class HW3UITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    // 1.A All elements exist and are reachable from the tests.
+    // NOTE: Try and get this working first.
+    // Points: 20
+    func testAllElementsExist() {
+        
         let app = XCUIApplication()
         app.launch()
+        
+        createScreenshotAndAttach()
+        
+                XCTAssertTrue(app.buttons["0"].exists)
+        XCTAssertTrue(app.buttons["1"].exists)
+        XCTAssertTrue(app.buttons["2"].exists)
+                XCTAssertTrue(app.buttons["3"].exists)
+        XCTAssertTrue(app.buttons["4"].exists)
+        XCTAssertTrue(app.buttons["5"].exists)
+        XCTAssertTrue(app.buttons["6"].exists)
+        XCTAssertTrue(app.buttons["7"].exists)
+        XCTAssertTrue(app.buttons["8"].exists)
+        XCTAssertTrue(app.buttons["9"].exists)
+        
+        XCTAssertTrue(app.buttons["+"].exists)
+        XCTAssertTrue(app.buttons["="].exists)
+        XCTAssertTrue(app.buttons["Clear"].exists)
+    
+        XCTAssertTrue(app.staticTexts["Result"].exists)
+        XCTAssertTrue(app.staticTexts["Result"].label == "0")
+    }
+    
+    // 1.B Number input
+    // Points: 10
+    func testTappingEachNumeralElementChangesTheResultLabel() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["9"].tap()
+        app.buttons["8"].tap()
+        app.buttons["7"].tap()
+        app.buttons["6"].tap()
+        app.buttons["5"].tap()
+        app.buttons["4"].tap()
+        app.buttons["3"].tap()
+        app.buttons["2"].tap()
+        app.buttons["1"].tap()
+        app.buttons["0"].tap()
+        
+        createScreenshotAndAttach()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "9876543210")
+    }
+    
+    // 1.C Clear button after number input
+    // Points: 10
+    func testTappingEachNumeralElementChangesTheResultLabelAndTappingClearResultsInDefaultValueOf0() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["9"].tap()
+        app.buttons["8"].tap()
+        app.buttons["7"].tap()
+        app.buttons["6"].tap()
+        app.buttons["5"].tap()
+        app.buttons["4"].tap()
+        app.buttons["3"].tap()
+        app.buttons["2"].tap()
+        app.buttons["1"].tap()
+        app.buttons["0"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+    }
+    
+    // 1.D
+    // NOTE: 15 + 17 = 32
+    // Points: 10
+    func testBasicAddOperation() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["1"].tap()
+        app.buttons["5"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        app.buttons["7"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "32")
+        
+        createScreenshotAndAttach()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    // 1.E
+    // NOTE: 0 + 17 = 17
+    // Points: 10
+    func testBasicAddOperation2() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        app.buttons["7"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "17")
+        
+        createScreenshotAndAttach()
+
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    // 1.F
+    // NOTE: 15 + 0 = 15
+    // Points: 10
+    func testBasicAddOperation3() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["1"].tap()
+        app.buttons["5"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "15")
+        
+        createScreenshotAndAttach()
+
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    // 1.G
+    // NOTE: 15 + 1 + 2 = 18
+    // Points: 10
+    func testBasicAddOperation4() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["1"].tap()
+        app.buttons["5"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["2"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "18")
+        
+        createScreenshotAndAttach()
+
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    // 1.H
+    // NOTE: 1 + 1 = 2 = 3
+    // Points: 10
+    func testBasicAddOperation5() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["1"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "2")
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "3")
+        
+        createScreenshotAndAttach()
+
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    // 1.I
+    // NOTE: 1 + 1 = 2 [CLEAR] + 15 = 15 [CLEAR] 0
+    // Points: 10
+    func testWildcardMath() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["1"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "2")
+
+        createScreenshotAndAttach()
+        
+        app.buttons["Clear"].tap()
+        
+        createScreenshotAndAttach()
+        
+        app.buttons["+"].tap()
+        
+        app.buttons["1"].tap()
+        
+        app.buttons["5"].tap()
+        
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "15")
+        
+        createScreenshotAndAttach()
+
+        app.buttons["Clear"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+        
+        createScreenshotAndAttach()
+    }
+    
+    private func createScreenshotAndAttach() {
+        
+        let mainScreenScreenshot = XCUIScreen.main.screenshot()
+        let attachment = XCTAttachment(screenshot: mainScreenScreenshot)
+        attachment.lifetime = .keepAlways
+        add(attachment)
+        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
+
